5318c41ab5c1c0f0a7f1f5a2b29b460b883cf401
elementAndSet <47096729+elementAndSet@users.noreply.github.com>
2019-11-22 18:08:22 +0900

    Update goimg.go

--- goimg.go
+++ goimg.go
@@ -28,6 +28,11 @@ type commonInfo struct {
 	xDiffSqAver float64
 }
 
+type fEnum struct {
+	f1 func(string, string, string) (commonInfo, interface{})
+	f2 func(string, string, string, string) commonInfo
+}
+
 //Flags :
 type Flags struct {
 	f0 bool //new folder
@@ -94,58 +99,6 @@ func main() {
 	}
 }
 
-/*
-func main() {
-	var commandsList [][]string
-	help := func() {
-		fmt.Println("    -f0 : create new folder and make result file in that")
-		fmt.Println(" 1Ddiff [-f0]  : get 1D diff image")
-		fmt.Println(" 2Ddiff [-f0]  : get 2D diff info image")
-		fmt.Println(" 2Dsimple [-f0]  : get 2D diff info clean image ")
-		fmt.Println(" info [-f0]  : get info CSV and JSON")
-		fmt.Println(" num : numbering jpeg andpng format file")
-		fmt.Println(" graph [-f0] : make Red-Green, Green-Blue, Red-Blue Graph")
-	}
-	if len(os.Args) < 2 {
-		fmt.Println("put your command")
-		help()
-	} else {
-		for mut := 1; mut < len(os.Args); mut++ {
-			if strings.HasPrefix(os.Args[mut], "-") == false {
-				var commands = []string{os.Args[mut]}
-				commandsList = append(commandsList, commands)
-			} else {
-				commandsList[len(commandsList)-1] = append(commandsList[len(commandsList)-1], os.Args[mut])
-			}
-		}
-		for _, v := range commandsList {
-			fun := v[0]
-			parameter := append([]string(nil), v[1:]...)
-			switch fun {
-			case "help":
-				help()
-			case "num":
-				imgNum("jpg", "jpeg", "JPG", "png", "PNG")
-			case "info":
-				imgInfo(parameter)
-			case "1Ddiff":
-				imgDiff1D(parameter)
-			case "2Ddiff":
-				imgDiff2D(parameter)
-			case "2Dsimple":
-				diff2DSimple(parameter)
-			case "graph":
-				imgGraph(parameter)
-			case "test":
-				returnFlags(parameter)
-			default:
-				fmt.Println(" wrong command!")
-			}
-		}
-	}
-}
-*/
-
 func returnFlags(op []string) Flags {
 	var Flag Flags
 	for _, v := range op {
@@ -164,7 +117,7 @@ func returnFlags(op []string) Flags {
 func imgInfo(options []string) []commonInfo {
 	flag := returnFlags(options)
 	list := mp.GetFilePathNameSlice("jpg", "jpeg", "JPG", "png")
-	infoList := []commonInfo{}
+	objects := []commonInfo{}
 	var format string
 	var imgInfoJSONSlice []img.ImgAver
 	for _, v := range list {
@@ -191,7 +144,7 @@ func imgInfo(options []string) []commonInfo {
 			YDiff: info.yDiffSqAver,
 		}
 		imgInfoJSONSlice = append(imgInfoJSONSlice, imgAveInfo)
-		infoList = append(infoList, info)
+		objects = append(objects, info)
 		fmt.Println(info.filename, " processed")
 	}
 	JSONIndent, _ := json.MarshalIndent(imgInfoJSONSlice, " ", "  ")
@@ -226,107 +179,75 @@ func imgInfo(options []string) []commonInfo {
 		each = each + "XDIIF:" + fmt.Sprintf("%f", v.XDiff) + ", " + "YDIFF:" + fmt.Sprintf("%f", v.YDiff) + "\n"
 		fCSV.WriteString(each)
 	}
-	return infoList
+	return objects
+}
+
+func metaImgInfo(option []string, objects []commonInfo) []commonInfo {
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string) (commonInfo, interface{}) {
+		var info commonInfo
+		info.filename = nameNoPointExtsn + extsnWithPoint
+		info.imageNRGBA = img.ReturnNRGBA(info.filename, deleExtsn)
+		info.averR, info.averG, info.averB, info.averA = img.ReturnRGBAAverage(info.imageNRGBA)
+		info.xDiff, info.yDiff = img.ReturnDiffInfo(info.imageNRGBA)
+		_, _, info.xDiffSqAver, info.yDiffSqAver = img.ReturnDiffSquareInfo(info.xDiff, info.yDiff)
+		imgAveInfo := img.ImgAver{
+			Name:  info.filename,
+			R:     info.averR,
+			G:     info.averG,
+			B:     info.averB,
+			A:     info.averA,
+			XDiff: info.xDiffSqAver,
+			YDiff: info.yDiffSqAver,
+		}
+		return info, imgAveInfo
+	}
+	fn := fEnum{f, nil}
+	return commonIter(option, objects, fn, "imgInfo")
 }
 
 func imgDiff1D(option []string, objects []commonInfo) []commonInfo {
-	flag := returnFlags(option)
-	var format string
-	Path, _ := os.Getwd()
-	var newFolder, xDiffNewName, yDiffNewName string
-	if flag.f0 {
-		newFolder = mp.MkDir("1Ddiff")
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string, newNameNoPointExtsn string) commonInfo {
+		var info commonInfo
+		NRGBA := img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, deleExtsn)
+		xDiff, yDiff := img.ReturnDiffInfo(NRGBA)
+		xDiffSq, yDiffSq, xDiffSqAver, yDiffSqAver := img.ReturnDiffSquareInfo(xDiff, yDiff)
+		xIrregDiffImg := img.ReturnIrregDiffImg(xDiffSq, [][]float64{}, (xDiffSqAver+yDiffSqAver)/2.0)
+		yIrregDiffImg := img.ReturnIrregDiffImg([][]float64{}, yDiffSq, (xDiffSqAver+yDiffSqAver)/2.0)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_XDIFF", deleExtsn, xIrregDiffImg)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_YDIFF", deleExtsn, yIrregDiffImg)
+		return info
 	}
-	for _, v := range objects {
-		Name, extsn := mp.GetFileNameAndExtsn(v.filename)
-		if extsn == ".jpg" || extsn == ".jpeg" || extsn == ".JPG" {
-			format = "jpeg"
-		} else if extsn == ".png" || extsn == ".PNG" {
-			format = "png"
-		} else {
-			format = extsn
-			continue
-		}
-
-		v.imageNRGBA = img.ReturnNRGBA(v.filename, format)
-		v.xDiff, v.yDiff = img.ReturnDiffInfo(v.imageNRGBA)
-		v.xDiffSq, v.yDiffSq, v.xDiffSqAver, v.yDiffSqAver = img.ReturnDiffSquareInfo(v.xDiff, v.yDiff)
-		if flag.f0 {
-			xDiffNewName = Path + "\\" + newFolder + "\\" + Name + "_XDIFF" + "."
-			yDiffNewName = Path + "\\" + newFolder + "\\" + Name + "_YDIFF" + "."
-		} else {
-			xDiffNewName = Name + "_XDIFF" + "."
-			yDiffNewName = Name + "_YDIFF" + "."
-		}
-		img.DrawFromNRGBA(xDiffNewName, format, img.ReturnIrregDiffImg(v.xDiffSq, [][]float64{}, v.xDiffSqAver))
-		img.DrawFromNRGBA(yDiffNewName, format, img.ReturnIrregDiffImg([][]float64{}, v.yDiffSq, v.xDiffSqAver))
-
-		fmt.Println(v.filename, " ", Name, " ", format)
-		fmt.Println(xDiffNewName, " ", yDiffNewName)
-	}
-	return objects
+	fn := fEnum{nil, f}
+	return commonIter(option, objects, fn, "1Ddiff")
 }
 
 func imgDiff2D(option []string, objects []commonInfo) []commonInfo {
-	var format string
-	flag := returnFlags(option)
-	path, _ := os.Getwd()
-	var newPath string
-	if flag.f0 || flag.f1 {
-		newPath = path + "\\" + mp.MkDir("2Ddiff") + "\\"
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string, newNameNoPointExtsn string) commonInfo {
+		var info commonInfo
+		NRGBA := img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, deleExtsn)
+		xDiff, yDiff := img.ReturnDiffInfo(NRGBA)
+		xDiffSq, yDiffSq, xDiffSqAver, yDiffSqAver := img.ReturnDiffSquareInfo(xDiff, yDiff)
+		IrregDiffImg := img.ReturnIrregDiffImg(xDiffSq, yDiffSq, (xDiffSqAver+yDiffSqAver)/2.0)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_2Ddiff", deleExtsn, IrregDiffImg)
+		return info
 	}
-	for _, v := range objects {
-		name, extsn := mp.GetFileNameAndExtsn(v.filename)
-		if extsn == ".jpg" || extsn == ".jpeg" || extsn == ".JPG" {
-			format = "jpeg"
-		} else if extsn == ".png" {
-			format = "png"
-		} else {
-			format = extsn
-			continue
-		}
-		if len(v.xDiff) == 0 || len(v.yDiff) == 0 {
-			fmt.Println(" recycle diff")
-			v.xDiff, v.yDiff = img.ReturnDiffInfo(img.ReturnNRGBA(v.filename, format))
-		}
-		if len(v.xDiffSq) == 0 || len(v.yDiffSq) == 0 || v.xDiffSqAver == 0.0 || v.yDiffSqAver == 0.0 {
-			fmt.Println(" RECYCLE DIFF")
-			v.xDiffSq, v.yDiffSq, v.xDiffSqAver, v.yDiffSqAver = img.ReturnDiffSquareInfo(v.xDiff, v.yDiff)
-		}
-		//v.xDiff, v.yDiff = img.ReturnDiffInfo(img.ReturnNRGBA(v.filename, format))
-		//v.xDiffSq, v.yDiffSq, v.xDiffSqAver, v.yDiffSqAver = img.ReturnDiffSquareInfo(v.xDiff, v.yDiff)
-		img.DrawFromNRGBA(newPath+name+"_graph", format, img.ReturnIrregDiffImg(v.xDiffSq, v.yDiffSq, (v.xDiffSqAver+v.yDiffSqAver/2.0)))
-		fmt.Println(name + "_graph." + format)
-	}
-	return objects
+	fn := fEnum{nil, f}
+	return commonIter(option, objects, fn, "2Ddiff")
 }
 
 func imgDiff2DSimple(option []string, objects []commonInfo) []commonInfo {
-	var format string
-	path, _ := os.Getwd()
-	flag := returnFlags(option)
-	var newPath string
-	if flag.f0 || flag.f1 {
-		newPath = path + "\\" + mp.MkDir("2DdiffSimplify") + "\\"
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string, newNameNoPointExtsn string) commonInfo {
+		var info commonInfo
+		NRGBA := img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, deleExtsn)
+		xDiff, yDiff := img.ReturnDiffInfo(NRGBA)
+		xDiffSq, yDiffSq, xDiffSqAver, yDiffSqAver := img.ReturnDiffSquareInfo(xDiff, yDiff)
+		averSq := (xDiffSqAver + yDiffSqAver) / 2.0
+		xDiffSqSimple, yDiffSqSimple := img.ReturnSimplify(xDiffSq, averSq), img.ReturnSimplify(yDiffSq, averSq)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_2Dsimple", deleExtsn, img.ReturnIrregDiffImg(xDiffSqSimple, yDiffSqSimple, averSq))
+		return info
 	}
-	for _, v := range objects {
-		name, extsn := mp.GetFileNameAndExtsn(v.filename)
-		if extsn == ".jpg" || extsn == ".jpeg" || extsn == ".JPG" {
-			format = "jpeg"
-		} else if extsn == ".png" {
-			format = "png"
-		} else {
-			format = extsn
-			continue
-		}
-		v.xDiff, v.yDiff = img.ReturnDiffInfo(img.ReturnNRGBA(v.filename, format))
-		v.xDiffSq, v.yDiffSq, v.xDiffSqAver, v.yDiffSqAver = img.ReturnDiffSquareInfo(v.xDiff, v.yDiff)
-		averSq := (v.xDiffSqAver + v.yDiffSqAver) / 2.0
-		xDiffSqSimple, yDiffSqSimple := img.ReturnSimplify(v.xDiffSq, averSq), img.ReturnSimplify(v.yDiffSq, averSq)
-		img.DrawFromNRGBA(newPath+name+"_graph", format, img.ReturnIrregDiffImg(xDiffSqSimple, yDiffSqSimple, averSq))
-		fmt.Println(name + "_graph." + format)
-	}
-	return objects
+	fn := fEnum{nil, f}
+	return commonIter(option, objects, fn, "2DdiffSimple")
 }
 
 func imgNum(extsn ...string) {
@@ -338,64 +259,45 @@ func imgNum(extsn ...string) {
 }
 
 func imgGraph(option []string, objects []commonInfo) []commonInfo {
-	var np, newName string
-	flag := returnFlags(option)
-	cp, _ := os.Getwd()
-	if flag.f0 || flag.f1 {
-		np = cp + "\\" + mp.MkDir("GraphImg")
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string, newNameNoPointExtsn string) commonInfo {
+		var info commonInfo
+		RGBAInfo := img.GetRGBAInfo(nameNoPointExtsn+extsnWithPoint, deleExtsn)
+		RG, GB, RB := img.MakeGraphBlueprint("", RGBAInfo)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_RGgraph", deleExtsn, RG)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_GBgraph", deleExtsn, GB)
+		img.DrawFromNRGBA(newNameNoPointExtsn+"_RBgraph", deleExtsn, RB)
+		return info
 	}
-	for _, v := range objects {
-		var EXTSN string
-		name, extsn := mp.GetFileNameAndExtsn(v.filename)
-		if extsn == ".jpg" || extsn == ".jpeg" || extsn == ".JPG" {
-			EXTSN = "jpeg"
-		} else if extsn == ".png" {
-			EXTSN = "png"
-		}
-		nameAndExtsn := fp.Base(v.filename)
-		RG, GB, RB := img.MakeGraphBlueprint(nameAndExtsn, img.GetRGBAInfo(nameAndExtsn, EXTSN))
-		if flag.f0 || flag.f1 {
-			newName = np + "\\" + name
-		} else {
-			newName = name
-		}
-		img.DrawFromNRGBA(newName+"_RGgraph", EXTSN, RG)
-		img.DrawFromNRGBA(newName+"_GBgraph", EXTSN, GB)
-		img.DrawFromNRGBA(newName+"_RBgraph", EXTSN, RB)
-	}
-	return objects
+	fn := fEnum{nil, f}
+	return commonIter(option, objects, fn, "ImgGraph")
 }
 
 func imgJ2P(option []string, objects []commonInfo) []commonInfo {
-	return commonIter(option, objects, metaJ2P, "J2Pfolder")
-}
-
-func metaJ2P() func(string, string, string) {
-	temp := func(nameNoPointExtsn string, extsnWithPoint string, newNameNoPointExtsn string) {
-		var delegateExtsn string
-		if extsnWithPoint == ".jpg" || extsnWithPoint == ".jpeg" || extsnWithPoint == ".JPG" {
-			delegateExtsn = "jpeg"
-		} else if extsnWithPoint == ".png" || extsnWithPoint == ".PNG" {
-			delegateExtsn = "png"
-		}
+	f := func(nameNoPointExtsn string, extsnWithPoint string, deleExtsn string, newNameNoPointExtsn string) commonInfo {
+		var info commonInfo
 		var newNRGBA *image.NRGBA
-		if delegateExtsn == "png" {
-			newNRGBA = img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, delegateExtsn)
-		} else if delegateExtsn == "jpeg" {
-			newNRGBA = img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, delegateExtsn)
+		if deleExtsn == "png" {
+			newNRGBA = img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, deleExtsn)
+		} else if deleExtsn == "jpeg" {
+			newNRGBA = img.ReturnNRGBA(nameNoPointExtsn+extsnWithPoint, deleExtsn)
 		}
 		img.DrawFromNRGBA(newNameNoPointExtsn, "png", newNRGBA)
+		info.imageNRGBA = newNRGBA
+		return info
 	}
-	return temp
+	fn := fEnum{nil, f}
+	return commonIter(option, objects, fn, "J2Pfolder")
 }
 
-func commonIter(option []string, objects []commonInfo, fn func() func(string, string, string), folderName string) []commonInfo {
+//func commonIter(option []string, objects []commonInfo, fn func(string, string, string, string) commonInfo, folderName string) []commonInfo {
+func commonIter(option []string, objects []commonInfo, fn fEnum, folderName string) []commonInfo {
 	var newPath, newNameNoPointExtsn string
 	flag := returnFlags(option)
 	cp, _ := os.Getwd()
 	if flag.f0 || flag.f1 {
 		newPath = cp + "\\" + mp.MkDir(folderName) + "\\"
 	}
+	var infoSlice []interface{}
 	for _, v := range objects {
 		nameNoPointExtsn, extsnWithPoint := mp.GetFileNameAndExtsn(v.filename)
 
@@ -404,7 +306,18 @@ func commonIter(option []string, objects []commonInfo, fn func() func(string, st
 		} else {
 			newNameNoPointExtsn = nameNoPointExtsn
 		}
-		fn()(nameNoPointExtsn, extsnWithPoint, newNameNoPointExtsn)
+		var deleExtsn string
+		if extsnWithPoint == ".jpg" || extsnWithPoint == ".jpeg" || extsnWithPoint == ".JPG" {
+			deleExtsn = "jpeg"
+		} else if extsnWithPoint == ".png" || extsnWithPoint == ".PNG" {
+			deleExtsn = "png"
+		}
+		if fn.f1 != nil {
+			_, element := fn.f1(nameNoPointExtsn, extsnWithPoint, deleExtsn)
+			infoSlice = append(infoSlice, element)
+		} else {
+			fn.f2(nameNoPointExtsn, extsnWithPoint, deleExtsn, newNameNoPointExtsn)
+		}
 	}
 	return objects
 }
